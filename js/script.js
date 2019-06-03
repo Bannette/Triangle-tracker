c818e88eb654b221b9b324bd9e0af20009213b51
Bannette <banetaloo3@gmail.com>
2019-06-03 19:21:21 +0300

    modified index and script files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function typeOfTriangle() {
+    document.querySelector("#side1").value;
+    document.querySelector("#side2").value;
+    document.querySelector("#side3").value;
+
+
+    if (side1==side2 && side2==side3){
+        console.log("equi")
+        alert('equil')
+    }
+}
