8c6030b7c337bf04cc303c225bd87b702e4416bc
Bannette <banetaloo3@gmail.com>
2019-06-04 22:47:08 +0300

    added funtions to my triangle tracker

--- js/script.js
+++ js/script.js
@@ -3,13 +3,44 @@ function typeOfTriangle(){
 	var side1= document.querySelector('#triangle-side1').value;
 	var side2= document.querySelector('#triangle-side2').value;
 	var side3= document.querySelector('#triangle-side3').value;
-	var answer=document.querySelector('#answer');
+    
 
-	// console.log('clickes')
-	if (side1==side2 && side2==side3){
-		console.log('equal')
-		 window.alert('equilateral')
-		answer.innerHTML = 'equilateral'
+	
+    var result=document.querySelector('#result')
+    if (side1<=0 || side2<=0 || side3<=0)
+    {
+        result.innerHTML=('<h2>Value Cannot be a triangle');
+        alert("check your values")
+    
+    }
+    else if 
+    (side1<=0 && side2<=0 && side3<=0)
+    {
+        result.innerHTML=('<h2>Value Cannot be a triangle');
+        alert("values cannot make a triangle")
+
+    }
+    else if
+    (side1===side2 && side2===side3 ){
+    result.innerHTML=('<h2>Equilateral triangle</h2>');
+    alert("Equilateral triangle ")
+    
+    }
+    else if(side1===side2 ||side2===side3 || side1===side3){
+        result.innerHTML=(' <h2>Isoseles triangle</h2>');
+        alert("Isosceles triangle")
     }
     
+    else if(side3 + side2 > side1 || side1 + side3 > side2 || side1 + side2 > side3){
+        result.innerHTML=(' <h2>Scalene triangle</h2>');
+        alert("scalene triangle")
+    
+    }
+   else{
+       (side1+side2 <= side3 || side2+side3 <= side1 || side3+side1 <= side2)
+           alert("not a triangle");
+       }
+   }
+  function refresh(){
+    location.reload();
 }
