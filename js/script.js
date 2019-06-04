c828187e618e5253db189acff63d1feb18e7f89e
Bannette <banetaloo3@gmail.com>
2019-06-04 22:48:33 +0300

    added rows and columns

--- js/script.js
+++ js/script.js
@@ -1,46 +1,28 @@
 function typeOfTriangle(){
 
-	var side1= document.querySelector('#triangle-side1').value;
-	var side2= document.querySelector('#triangle-side2').value;
-	var side3= document.querySelector('#triangle-side3').value;
+	var side1= document.querySelector('#side1').value;
+	var side2= document.querySelector('#side2').value;
+	var side3= document.querySelector('#side3').value;	
     
-
-	
     var result=document.querySelector('#result')
-    if (side1<=0 || side2<=0 || side3<=0)
-    {
-        result.innerHTML=('<h2>Value Cannot be a triangle');
-        alert("check your values")
-    
-    }
-    else if 
-    (side1<=0 && side2<=0 && side3<=0)
-    {
-        result.innerHTML=('<h2>Value Cannot be a triangle');
-        alert("values cannot make a triangle")
+  
+    var arrSide = [side1, side2, side3];
 
-    }
-    else if
-    (side1===side2 && side2===side3 ){
-    result.innerHTML=('<h2>Equilateral triangle</h2>');
-    alert("Equilateral triangle ")
+    if (side1 > 0 && side2 > 0 && side3 > 0) {
+      if (side1 === side2 && side2 === side3) {
+        alert("Your values make an equilateral triangle. All sides equal;");
+      }
+      else if (side1 === side2 || side1 === side3 || side2 === side3) {
+        alert("Your values give an isosceless triangle. Two sides equal;");
+      } 
+    
+      else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+        alert("Your values give an scalene triangle. all sides not equal;");
+      }
     
     }
-    else if(side1===side2 ||side2===side3 || side1===side3){
-        result.innerHTML=(' <h2>Isoseles triangle</h2>');
-        alert("Isosceles triangle")
+     else {
+         (((side1 + side2) <= side3) || ((side1 + side3) <= side2) || ((side2 + side3) <= side1))
+      alert("Insert a number greater than zero, values can't make a triangle");
     }
-    
-    else if(side3 + side2 > side1 || side1 + side3 > side2 || side1 + side2 > side3){
-        result.innerHTML=(' <h2>Scalene triangle</h2>');
-        alert("scalene triangle")
-    
-    }
-   else{
-       (side1+side2 <= side3 || side2+side3 <= side1 || side3+side1 <= side2)
-           alert("not a triangle");
-       }
-   }
-  function refresh(){
-    location.reload();
-}
+  }
